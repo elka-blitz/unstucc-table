2ea881e17ab2ab17b0cf83e158d663f93362ed9c
elka-blitz <122494314@umail.ucc.ie>
2025-09-21 17:23:45 +0100

    Separate days into dict

--- main.py
+++ main.py
@@ -30,3 +30,21 @@ for tt_line in table_text:
     index += 1
 
 print(day_indexes)
+
+# Get the lines between the indexes, assign to day.
+# Days are constant anyway, so can be hardcoded
+
+sched_per_day = {}
+
+sched_per_day['Mon'] = [day_indexes[0], day_indexes[1]]
+sched_per_day['Tue'] = [day_indexes[1], day_indexes[2]]
+sched_per_day['Wed'] = [day_indexes[2], day_indexes[3]]
+sched_per_day['Thu'] = [day_indexes[3], day_indexes[4]]
+sched_per_day['Fri'] = [day_indexes[4], len(table_text)-1]
+
+print(sched_per_day)
+# There is the option of incorporating Saturday, potential for an endpoint
+
+for day in sched_per_day:
+    print(sched_per_day[day][1] - sched_per_day[day][0])
+
